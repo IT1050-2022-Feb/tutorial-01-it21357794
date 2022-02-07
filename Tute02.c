0e48452aa29270d13c059b54d4b38dcf0c16918d
it21357794 <99163339+it21357794@users.noreply.github.com>
2022-02-07 14:49:27 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,18 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  float distance,amount;
   
+  printf("\nEnter the distance(km):");
+  scanf("%f",distance);
+  
+  if(distance<=30 && distance>0){
+    amount=(float)  distance*50;
+  }
+  else if(distance>30){
+    amount= (float) (30*50)+((distance-30)*40);
+  }
+  
+  printf("\nAmount=%.2f",amount);
   return 0;
 }
